56dd8600adf485fa395c04ad4f9bc5164d402ede
ditto7890 <kwak071219@gmail.com>
2020-04-23 21:39:41 +0900

    .

--- entcord.js
+++ entcord.js
@@ -1,9 +1,9 @@
-let token, client = null;
+let client = null;
+
 $.get("https://raw.githack.com/discordjs/discord.js/webpack/discord.master.min.js").done(e => {
     client = new Discord.Client()
 });
 
 if (Entry.variableContainer.getVariableByName('EntTube').getValue() && client) {
-    token = Entry.variableContainer.getVariableByName('EntTube').getValue()
-    client.login(token).then(e => alert(`${client.user.tag} is Login`)).catch(err=>alert(`Error...\n${err}`))
+    client.login(Entry.variableContainer.getVariableByName('EntTube').getValue()).then(e => alert(`${client.user.tag} is Login`)).catch(err=>alert(`Error...\n${err}`))
 }
