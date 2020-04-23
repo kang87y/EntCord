b1169c05235ba23dd4abc7134c38484faf5f1d81
ditto7890 <kwak071219@gmail.com>
2020-04-23 21:50:27 +0900

    ...

--- entcord.js
+++ entcord.js
@@ -4,6 +4,6 @@ $.get("https://raw.githack.com/discordjs/discord.js/webpack/discord.master.min.j
     client = new Discord.Client()
 });
 
-if (Entry.variableContainer.getVariableByName('토큰').getValue() && client) {
+if (!Entry.variableContainer.getVariableByName('토큰').getValue() === "null" && client) {
     client.login(Entry.variableContainer.getVariableByName('토큰').getValue()).then(e => alert(`${client.user.tag} is Login`)).catch(err=>alert(`Error...\n${err}`))
 }
